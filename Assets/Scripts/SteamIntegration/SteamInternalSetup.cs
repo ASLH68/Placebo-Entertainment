0dee525c9384710165e1ee7ae46aa258eab831e0
AndreaXyna <andreaswihartdecoster@gmail.com>
2025-04-04 14:33:31 -0500

    Initial steam setup
    - Added steam manager
    - Began setting up authentication for testing

---
+++ Assets/Scripts/SteamIntegration/SteamInternalSetup.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using Steamworks;
+using UnityEngine;
+
+public class SteamInternalSetup : MonoBehaviour
+{
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+    public class SteamScript : MonoBehaviour {
+        protected Callback<GameOverlayActivated_t> m_GameOverlayActivated;
+
+        private void OnEnable() {
+            if (SteamManager.Initialized) {
+                m_GameOverlayActivated = Callback<GameOverlayActivated_t>.Create(OnGameOverlayActivated);
+            }
+        }
+
+        private void OnGameOverlayActivated(GameOverlayActivated_t pCallback) {
+            if(pCallback.m_bActive != 0) {
+                Debug.Log("Steam Overlay has been activated");
+            }
+            else {
+                Debug.Log("Steam Overlay has been closed");
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/SteamIntegration/SteamScript.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Steamworks;
+
+public class SteamScript : MonoBehaviour {
+    void Start() {
+        if(SteamManager.Initialized) {
+            string name = SteamFriends.GetPersonaName();
+            Debug.Log(name);
+        }
+    }
+}
